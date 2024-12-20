952c549ad2567aa7888673de9a446f5ee79a3209
msetitech <msetitech@yahoo.com>
2024-12-20 09:32:36 +0300

    adding Display info Function

--- store.cpp
+++ store.cpp
@@ -4,10 +4,15 @@
 
 using namespace std;
 
-struct store {
+struct items {
     string name;
     float price;
     int id;
 
-    
+    // display function
+    void displayProducts(){
+        cout << "\n---- ITEM MANAGEMENT SYSTEM ----\n";
+        cout << "Product ID: " << id << ", Product Name: " << name << "Product Price: " << price << endl;
+    }
+
 };
